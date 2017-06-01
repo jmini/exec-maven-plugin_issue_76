a045801fb19170f055d5c6d0556f9184055c1396
Jeremie Bresson <jeremie.bresson@bsi-software.com>
2017-06-01 18:17:47 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # exec-maven-plugin_issue_76
-Example project for mojohaus/exec-maven-plugin #76
+
+Example project for mojohaus/exec-maven-plugin [#76](https://github.com/mojohaus/exec-maven-plugin/issues/76)
---
+++ my-java-project/src/main/java/my/java/project/JavaExample.java
@@ -0,0 +1,10 @@
+package my.java.project;
+
+public class JavaExample {
+
+  static final String HELLO_WORLD = "Hello World!";
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(HELLO_WORLD);
+  }
+}
